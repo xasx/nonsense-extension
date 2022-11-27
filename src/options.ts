db16ee6205b2687625998c6191f62c9fe8bf8e12
Andreas Schöneck <as.maps@gmail.com>
2022-11-27 18:31:23 +0100

    Add more options

--- src/options.ts
+++ src/options.ts
@@ -4,20 +4,59 @@ function saveSettings() {
             return;
         }
 
+    const zoomfactor = document.getElementById("zoomfactor") as HTMLInputElement;
 
-    let settings = { mode: mode.value };
+    if (zoomfactor === null || !isFinite(Number(zoomfactor.value))) {
+        setStatus("Invalid Zoom factor value");
+        return;
+    }
+
+    const delay = document.getElementById("delay") as HTMLInputElement;
+
+    if (delay === null || !isFinite(Number(delay.value))) {
+        setStatus("Invalid value for Delay");
+        return;
+    }
+
+    const showOptions = document.getElementById("showOptions") as HTMLInputElement;
+
+    const blacklist = document.getElementById("blacklist") as HTMLTextAreaElement;
+    const blacklistItems = blacklist.value.split("\n").sort().filter(bli => bli.length > 0);
+
+    console.log("black list", blacklistItems);
+    blacklist.value = blacklistItems.join("\n");
+
+    let settings = { mode: mode.value, zoomfactor: zoomfactor.value, delay: delay.value, showOptions: showOptions.checked, blacklistItems };
     chrome.storage.local.set(settings, function() {
-        console.log('Settings saved.');
-        const status = document.getElementById("status") as HTMLDivElement;
-        status.textContent = "Settings saved.";
+        console.log('Settings saved');
+        setStatus("Settings saved.");
     });
 }
 
+function setStatus(statusText: string) {
+    const status = document.getElementById("status") as HTMLDivElement;
+
+    status.textContent = statusText;
+}
+
 function loadSettings() {   
-    chrome.storage.local.get('mode', function(data) {
+    chrome.storage.local.get(['mode', "zoomfactor", "delay", "showOptions", "blacklistItems"], function(data) {
         
         const mode = document.getElementById("m" + data.mode) as HTMLInputElement;
         mode.checked = true;
+
+        const zoomfactor = document.getElementById("zoomfactor") as HTMLInputElement;
+        zoomfactor.value = data.zoomfactor;
+
+        const delay = document.getElementById("delay") as HTMLInputElement;
+        delay.value = data.delay;
+
+        const showOptions = document.getElementById("showOptions") as HTMLInputElement;
+        showOptions.checked = data.showOptions;
+
+        const blacklist = document.getElementById("blacklist") as HTMLTextAreaElement;
+        blacklist.value = data.blacklistItems.join("\n");
+
         document.getElementById("status").textContent = "Settings loaded.";
     });
 }
